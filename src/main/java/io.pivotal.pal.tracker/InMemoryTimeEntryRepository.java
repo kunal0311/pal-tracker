5a52c299eb2447784fff370eb51157b0859e22f0
Kunal Makwana <kunal_makwana@syntelinc.com>
2018-05-15 11:15:52 -0700

    Added Spring MVC files

---
+++ src/main/java/io.pivotal.pal.tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+    private HashMap<Long, TimeEntry> timeEntries = new HashMap<>();
+
+    @Override
+    public TimeEntry create(TimeEntry timeEntry) {
+        long id = timeEntries.size() + 1L;
+        TimeEntry timeEntryNew = new TimeEntry(id, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours());
+        timeEntries.put(id, timeEntryNew);
+        return timeEntryNew;
+    }
+
+    @Override
+    public TimeEntry find(long id) {
+        TimeEntry timeEntry = timeEntries.get(id);
+        return timeEntry;
+    }
+
+    @Override
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        TimeEntry timeEntryUpdate = new TimeEntry(id, timeEntry.getProjectId(), timeEntry.getUserId(), timeEntry.getDate(), timeEntry.getHours());
+        timeEntries.replace(id, timeEntryUpdate);
+        return timeEntryUpdate;
+    }
+
+    @Override
+    public void delete(long id) {
+        timeEntries.remove(id);
+    }
+
+    @Override
+    public ArrayList list() {
+        return new ArrayList<>(timeEntries.values());
+    }
+}
